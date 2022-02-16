96bff3678e1f3a05812974cb8fe7f84fc3fa981e
Peter Ivony <peterivony@protonmail.com>
2022-02-16 01:41:48 +0100

    Add files via upload

---
+++ sources/sql/drop.sql
@@ -0,0 +1,18 @@
+DROP VIEW LOWEST_PAID_TECHNIKER;
+DROP VIEW FIRST_20_MOVIES;
+DROP TRIGGER Kinosaal_Checker;
+DROP TABLE betreuen;
+DROP TABLE verweist_auf;
+DROP TABLE Buchung;
+DROP TABLE Kunde;
+DROP TABLE Reinigungspersonal;
+DROP TABLE Kassierer;
+DROP TABLE Techniker;
+DROP TABLE MitarbeiterIn;
+DROP TABLE Anzeige;
+DROP TABLE Sitzplatz;
+DROP TABLE Kinosaal;
+DROP TABLE Kino_Filiale;
+DROP SEQUENCE mitarbeiter_nr_gen;
+
+commit;
